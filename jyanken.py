ab4ac1f337b4bbaa6ae57267fa9d7c15ea4df49b
ryatajoshima <sojoy8@gmail.com>
2016-12-06 12:16:25 +0900

    second commit

--- jyanken.py
+++ jyanken.py
@@ -1,5 +1,5 @@
 import utils
-# randomモジュールを読み込んでください
+
 import random
 
 print('じゃんけんをはじめます')
@@ -8,7 +8,7 @@ print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
 player_hand = int(input('数字で入力してください：'))
 
 if utils.validate(player_hand):
-    # randintを用いて0から2までの数値を取得し、変数computer_handに代入してください
+
     computer_hand = random.randint(0, 2)
 
 
